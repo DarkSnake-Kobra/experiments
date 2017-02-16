e9248b232339af01c9f3d91f6a879df86afc7e6f
DarkSnake-Kobra <jbttoms@gmail.com>
2017-02-16 07:43:58 -0600

    Signed-off-by: DarkSnake-Kobra <jbttoms@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Details
 These are small programs I made for testing out various features in projects. It's also for reference.
+
+
+# Build Commands
+* electron-packager . --platform=win32 --arch=x64
+* sudo ./node_modules/.bin/electron-packager . --platform=linux --arch=x64
--- readfilesync/app.js
+++ readfilesync/app.js
@@ -1,19 +1,36 @@
 var fs = require('fs');
+var path;
 
 
 function readConfig()
 {
-	var config = fs.readFileSync('test.conf', 'utf8');
-    config = config.parseInt();
-    alert(config);
+    path =__dirname + "\\"+ "data" + "\\" + "test.conf";
+    log();
+    temp();
 }
 function about()
 {
 	alert("Copyright (c) 2017 Jordan Bottoms\n" +
 	"Released under the BSD 3 Clause License\n" +
 	"Website: jordanbottoms.com");
+}
 
 function closeWin()
 {
 	window.close();
+}
+
+function temp()
+{
+    var config = fs.readFileSync(path, 'utf8');
+    config = config.parseInt();
+    alert(config);
+}
+
+function log()
+{
+    fs.appendFile('log', path,(err) => {
+    if (err) throw err;
+    console.log('The "data to append" was appended to file!');
+    });
 }
