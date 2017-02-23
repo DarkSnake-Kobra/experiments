df2a27bc17f867053120491d92fe9e7faa6b53c2
DarkSnake-Kobra <jbttoms@gmail.com>
2017-02-23 09:12:48 -0600

    should now read config file
    Signed-off-by: DarkSnake-Kobra <jbttoms@gmail.com>

--- readfilesync/app.js
+++ readfilesync/app.js
@@ -1,12 +1,11 @@
 var fs = require('fs');
-var path;
 
 
 function readConfig()
 {
-    path =__dirname + "\\" + "data" + "\\"+ test.conf";
-    log();
-    temp();
+	configPath =__dirname + "\\" + "data" + "\\" + "config.conf";
+    var config = fs.readFileSync(configPath, 'utf8');
+    document.getElementById('number').value= config;
 }
 function about()
 {
@@ -18,19 +17,4 @@ function about()
 function closeWin()
 {
 	window.close();
-}
-
-function temp()
-{
-    var config = fs.readFileSync(path, 'utf8');
-    config = config.parseInt();
-    alert(config);
-}
-
-function log()
-{
-    fs.appendFile('log', path,(err) => {
-    if (err) throw err;
-    console.log('The "data to append" was appended to file!');
-    });
 }
